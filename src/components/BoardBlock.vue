e709385a2593d796af67974f6ea6b41d8bdf1648
hogeun1999 <cgh100039@gmail.com>
2025-03-07 00:15:57 +0900

    make chess board and set basic state

---
+++ src/components/BoardBlock.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="board-block">
+    <div :class="evenOrOdd" @click="handleOnClick">
+      <img class="img" :src="imgURL" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+
+export default Vue.extend({
+  name: 'BoardBlock',
+  methods: {
+    handleOnClick() {
+      console.log('click')
+    }
+  },
+  props: {
+    value:{
+      type: String,
+      required: true,
+    },
+    index:{
+      type: Number,
+      required: true,
+    }
+  },
+  computed:{
+    evenOrOdd(): String{
+      if (Math.floor(this.index / 8) % 2 === this.index % 2) {
+        return 'board-block-even';
+      } else {
+        return 'board-block-odd';
+      }
+    },
+    imgURL(): String | undefined{
+      if(this.value !== ''){
+        return `https://assets-themes.chess.com/image/tlwah/150/${this.value}.png`;
+      }
+      else{
+        return undefined
+      }
+    }
+  },
+});
+</script>
+
+<style scoped>
+.board-block{
+  width: 90px;
+  height: 90px;
+}
+.board-block-even{
+  width: 100%;
+  height: 100%;
+  background-color: green;
+}
+
+.board-block-odd{
+  width: 100%;
+  height: 100%;
+  background-color: white;
+}
+
+.img{
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/ChessBoard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="chess-board"> 
+    <BoardBlock v-for="(board,index) in flatBoardData" :key="index" :value="board" :index="index"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BoardBlock from './BoardBlock.vue';
+
+const boardData: string[][] = [
+  ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'],
+  ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+  ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
+];
+
+const flatBoardData: string[] = boardData.flat();
+
+export default Vue.extend({
+  name: 'ChessBoard',
+  components: {
+    BoardBlock,
+  },
+  data() {
+    return {
+      flatBoardData,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.chess-board{
+  border: 1px solid black;
+  width: 720px;
+  height: 720px;
+  display: grid;
+  grid-template-columns: repeat(8, 90px);
+  grid-template-rows: repeat(8, 90px);
+}
+</style>
